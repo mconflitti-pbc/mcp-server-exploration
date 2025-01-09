5f20c0755f0269d65cb53cbeff3960387b733d58
Barret Schloerke <barret@posit.co>
2025-01-09 14:47:17 -0500

    Use an app for `make client`

--- app.py
+++ app.py
@@ -13,16 +13,23 @@ async def main():
     mcp_client = MCPClient(llm)
     await mcp_client.register_mcp_server("http://127.0.01:8082/sse")
 
-    # await mcp_client.chat_async("What is the current Connect host version?")
-    await mcp_client.chat_async("Get the current user's id.")
-    # await mcp_client.chat_async("Now get me the first 3 content items owned by that user id.")
+    if True:
+        mcp_client.llm.app(bg_thread=True)
 
-    # Works! Just commenting for now
-    # await mcp_client.chat_async("Update that user's first name to `Testing`")
+        await asyncio.sleep(1000)
+    else:
+        # await mcp_client.chat_async("What is the current Connect host version?")
+        await mcp_client.chat_async("Get the current user's id.")
+        # await mcp_client.chat_async("Now get me the first 3 content items owned by that user id.")
 
-    mcp_client.llm.export("output.md", overwrite=True, include="all", include_system_prompt=True)
+        # Works! Just commenting for now
+        # await mcp_client.chat_async("Update that user's first name to `Testing`")
 
-    await mcp_client.cleanup()
+        mcp_client.llm.export(
+            "output.md", overwrite=True, include="all", include_system_prompt=True
+        )
+
+        await mcp_client.cleanup()
 
 
 if __name__ == "__main__":
