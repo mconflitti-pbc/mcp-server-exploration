9919c27d091570a4337f33246a37d57cc4168805
Barret Schloerke <barret@posit.co>
2025-01-09 14:48:41 -0500

    Clean up app. Add useful init loading for faster debugging

--- shiny/app.py
+++ shiny/app.py
@@ -4,6 +4,7 @@
 # To get started, follow the instructions at https://aws.amazon.com/bedrock/claude/
 # as well as https://github.com/anthropics/anthropic-sdk-python#aws-bedrock
 # ------------------------------------------------------------------------------------
+import asyncio
 import os
 from typing import Any
 
@@ -17,11 +18,10 @@ from typing_extensions import TYPE_CHECKING
 # from anthropic import AnthropicBedrock
 from mcp_servers.helpers.swagger import (
     OperationDef,
-    expand_swagger,
+    expand_all_references,
     transform_swagger_to_operation_dict,
 )
 from openapi_mcp.connect_api import (
-    handle_operation,
     make_request,
     map_arguments_to_api_params,
     map_operations_to_tools,
@@ -53,9 +53,6 @@ chat_ui = ui.Chat(id="chat")
 async def _():
     user_input = chat_ui.user_input()
 
-    print("Tools:", chat._tools)
-    print("STREAM_CHAT:", STREAM_CHAT)
-
     if user_input is None:
         return
     if STREAM_CHAT:
@@ -64,97 +61,26 @@ async def _():
         await chat_ui.append_message(str(chat.chat(user_input)))
 
 
-# llm = anthropic.AnthropicBedrock(
-#     # aws_secret_key=os.getenv("AWS_SECRET_KEY"),
-#     # aws_access_key=os.getenv("AWS_ACCESS_KEY"),
-#     aws_region=aws_region,
-#     # aws_account_id=os.getenv("AWS_ACCOUNT_ID"),
-# )
-
-# def make_func(operation) -> Callable:
-
-#     class SimpleObject:
-#         ...
-#     class ComplicatedObject:
-#         inner: SimpleObject
-#         ...
-
-
-#     def ret_fn(...) -> ComplicatedObject:
-#         handle_operation(SUPPORTED_OPERATIONS, name, arguments)
-
-#     ret_fn.__doc__ = operation["description"]
-#     return {fn: ret_fn, classes: [SimpleObject, ComplicatedObject]}
-
-
-# @chat_ui.on_user_submit
-# async def _():
-#     # Get messages currently in the chat
-#     messages = chat_ui.messages(format="anthropic")
-#     # Create a response message stream
-#     # print("anthropic_tools()", anthropic_tools())
-#     response = llm.messages.create(
-#         model=aws_model,
-#         messages=messages,
-#         stream=STREAM_CHAT,
-#         max_tokens=1000,
-#         tools=anthropic_tools(),
-#     )
-
-#     llm.messages.
-
-#     if STREAM_CHAT:
-#         # Append the response stream into the chat
-#         await chat_ui.append_message_stream(response)
-#     else:
-#         # Append the response as a single message
-#         await chat_ui.append_message(response)
-
-
-# class ToolParam(TypedDict, total=False):
-#     input_schema: Required[InputSchema]
-#     """[JSON schema](https://json-schema.org/) for this tool's input.
-
-#     This defines the shape of the `input` that your tool accepts and that the model
-#     will produce.
-#     """
-
-#     name: Required[str]
-#     """Name of the tool.
-
-#     This is how the tool will be called by the model and in tool_use blocks.
-#     """
-
-#     cache_control: Optional[CacheControlEphemeralParam]
-
-#     description: str
-#     """Description of what this tool does.
-
-#     Tool descriptions should be as detailed as possible. The more information that
-#     the model has about what the tool is and how to use it, the better it will
-#     perform. You can use natural language descriptions to reinforce important
-#     aspects of the tool input JSON schema.
-#     """
-
-
-@reactive.calc
-def anthropic_tools():
-    return [
-        {
-            "input_schema": tool.inputSchema,
-            "name": tool.name,
-            "description": tool.description,
-        }
-        for tool in openapi_tools()
-    ]
+# @reactive.calc
+# def anthropic_tools():
+#     return [
+#         {
+#             "input_schema": tool.inputSchema,
+#             "name": tool.name,
+#             "description": tool.description,
+#         }
+#         for tool in openapi_tools()
+#     ]
 
 
 class RawChatlasTool(chatlas.Tool):
+    # TODO-chatlas: Add this method to the chatlas.Chat class
     @staticmethod
     def reset_tools(chat: chatlas.Chat):
         chat._tools = {}
         return
 
+    # TODO-chatlas: Add this method to the chatlas.Chat class
     @staticmethod
     def register_tool(chat: chatlas.Chat, tool: "RawChatlasTool"):
         chat._tools[tool.name] = tool
@@ -168,22 +94,13 @@ class RawChatlasTool(chatlas.Tool):
         operation_input_schema = tool.inputSchema
 
         async def call_api(**kwargs: Any):
-            print("\n\nCalling tool", self.name, "with args:", kwargs)
-            # handle_operation({self.name: }, self.name, kwargs)
-
-            # print(f"Calling {name} with args: {arguments}")
-            # operation = operations[name]
+            # print("\n\nCalling tool", self.name, "with args:", kwargs)
             api_params = map_arguments_to_api_params(
                 kwargs,
                 operation["definition"].get("parameters", []),
             )
-
-            print("Calling operation: ", operation, api_params)
             result = await make_request(base_url, operation, api_params)
-            # print("Received Result")
-            print("Received Result: `{result}`")
             return [mcp_types.TextContent(text=result, type="text")]
-            return "hello barret!"
 
         super().__init__(call_api, model=None)
 
@@ -209,102 +126,49 @@ def _():
     if not api_operations:
         req(api_operations)
         return
-    print("Registering operations!", [operation["name"] for operation in api_operations.values()])
-    print(api_operations)
-
-    # _ = [
-    #     {
-    #         "input_schema": {"type": "object", "properties": {}, "required": []},
-    #         "name": "read_root__get",
-    #         "description": "Root endpoint returning a simple message.",
-    #     },
-    #     {
-    #         "input_schema": {
-    #             "type": "object",
-    #             "properties": {
-    #                 "item_id": {"type": "integer", "description": "(No description provided)"},
-    #                 "query_param": {"type": "string", "description": "(No description provided)"},
-    #             },
-    #             "required": ["item_id"],
-    #         },
-    #         "name": "read_item_items__item_id__get",
-    #         "description": "Endpoint to retrieve item details.",
-    #     },
-    # ]
-
-    # input_schema = {
-    #     "type": "function",
-    #     "function": {
-    #         "name": tool["name"],
-    #         "description": tool["description"],
-    #         "parameters": tool["input_schema"],
-    #     },
-    # }
 
     api_url = openapi_url.get()
 
     RawChatlasTool.reset_tools(chat)
     for operation in api_operations.values():
-        print("operation", operation)
         RawChatlasTool.register_tool(
             chat,
             RawChatlasTool(
-                # fn=fn,
                 base_url=api_url,
                 operation=operation,
             ),
         )
 
-    print("Tools registered!", chat._tools)
 
+with ui.sidebar(open="open", id="sidebar", width="50%"):
+    with ui.accordion(id="acc", open="chat_panel"):
+        with ui.accordion_panel("Available Tools", value="available_tools_panel"):
 
-# @reactive.effect
-# def _():
-#     operations = openapi_operations()
-#     ui.notification_show("Making tools", id="making_tools")
+            @render.ui
+            def _openapi_tools():
+                tools = [
+                    core_ui.TagList(
+                        core_ui.tags.dt(tool.name),
+                        # ": ",
+                        core_ui.tags.dl(core_ui.tags.pre(tool.description)),
+                    )
+                    for tool in openapi_tools()
+                ]
+                return core_ui.tags.dl(*tools)
 
-#     funcs = []
-#     for operation in operations.values():
+            @reactive.effect
+            def _():
+                ui.update_accordion_panel(
+                    "acc",
+                    "available_tools_panel",
+                    title=f"Available Tools ({len(openapi_tools())})",
+                )
 
-#         def make_func(operation):
-#             async def _call(**args):
-#                 result = await chat.stream(operation["name"], **args)
-#                 print(f"Called: {operation['name']}; Result: {result}")
-#                 return result.content[0].text
-
-#             return _call
-
-#         funcs.append(make_func(operation))
-
-#     # tools = openapi_tools()
-#     # ui.notification_show("Registering tools", id="registering_tools")
-#     # for tool in tools:
-#     #     chat._tools[tool.name] = tool
-#     #     # chat.register_tool(tool)
-
-
-with ui.sidebar(open="open", id="sidebar", title="LLM", width="50%"):
-
-    @render.ui
-    def _openapi_tools():
-        tools = [
-            core_ui.TagList(
-                core_ui.tags.dt(tool.name),
-                # ": ",
-                core_ui.tags.dl(tool.description),
+        with ui.accordion_panel("Chat", value="chat_panel"):
+            # Display chat
+            chat_ui.ui(
+                placeholder="Type here...",
             )
-            for tool in openapi_tools()
-        ]
-        ret = core_ui.TagList(
-            core_ui.tags.h3("Available Tools: ", len(tools)),
-            core_ui.tags.dl(*tools),
-        )
-        return ret
-
-    # Display chat
-    chat_ui.ui(
-        placeholder="Type here...",
-    )
 
 
 @render.ui
@@ -319,8 +183,6 @@ def _openapi_frame():
     )
     return ui.fill.as_fill_item(frame)
 
-
-swagger_url = reactive.value()
 
 modal = ui.modal(
     ui.input_text(
@@ -338,16 +200,6 @@ modal = ui.modal(
 @reactive.effect
 def _():
     ui.modal_show(modal)
-
-
-ui.HTML("""
-<script>
-    function click_submit() {
-        document.getElementById("api_url_submit").click();
-    };
-    setTimeout(click_submit, 1 * 1000);
-</script>
-        """)
 
 
 # defined to be end with `/`
@@ -369,14 +221,13 @@ def openapi_json():
         req(False)
         return
     result = response.json()
-    # ui.notification_remove("fetching_openapi")
     return result
 
 
 @reactive.calc
 def openapi_operations():
     ui.notification_show("Transforming OpenAPI schema to operations", id="transforming_openapi")
-    document = expand_swagger(openapi_json())
+    document = expand_all_references(openapi_json())
     operations = transform_swagger_to_operation_dict(document)
     return operations
 
@@ -386,26 +237,9 @@ def openapi_tools():
     return map_operations_to_tools(openapi_operations())
 
 
-@reactive.calc
-def chat_tools():
-    api_tools = openapi_tools()
-    if not api_tools:
-        req(api_tools)
-        return
-
-    ret_tools: RawChatlasTool = []
-
-
-# @reactive.effect
-# def _():
-#     # print("openapi", openapi())
-#     print("openapi_operations", openapi_operations())
-#     # print("openapi_tools", openapi_tools())
-
-
 @reactive.effect
 @reactive.event(input.api_url_submit)
-def _():
+async def _():
     input_api_url = input.api_url()
     if input_api_url is None:
         return
@@ -422,3 +256,58 @@ def _():
 
     openapi_url.set(input_api_url)
     ui.modal_remove()
+
+
+# Helpful debugging setup for interactive / non-deployed mode
+# if not os.environ.get("CONNECT_CONTENT_GUID", ""):
+if False:
+    ui.HTML("""
+<script>
+    function click_submit() {
+        document.getElementById("api_url_submit").click();
+    };
+    setTimeout(click_submit, 1 * 1000);
+</script>
+        """)
+
+    # When the url is set, wait a bit before setting the chat input and submit it
+    @reactive.effect
+    @reactive.event(openapi_url)
+    def _():
+        trigger_set_chat()
+
+    # Wait a bit
+    @reactive.extended_task
+    async def trigger_set_chat(delay: int = 1) -> int:
+        await asyncio.sleep(delay)
+        return delay
+
+    # Set the chat input and submit it
+    @reactive.effect
+    async def _():
+        # Only when the waited "result" is finished
+        trigger_set_chat.result()
+
+        chat_ui.update_user_input(
+            # value="Hello"
+            # value="who is the tallest person in Luke Skywalker's extended (max distance of 2 people) family tree (including force relationships)?"
+            value="who is the tallest person in Luke Skywalker's immidiate family?"
+        )
+
+        add_chat_entry = ui.HTML("""
+        <script>
+            setTimeout(async () => {
+                const enterEvent = new KeyboardEvent('keydown', {
+                    key: 'Enter',
+                    code: 'Enter',
+                    keyCode: 13,
+                    which: 13,
+                });
+
+                // Dispatch the 'Enter' event on the input element
+                console.log("Dispatching Enter event");
+                document.querySelector("textarea#chat_user_input").dispatchEvent(enterEvent);
+            }, 500);
+        </script>
+                """)
+        ui.insert_ui(add_chat_entry, "body")
